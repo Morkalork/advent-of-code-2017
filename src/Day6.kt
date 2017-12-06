e93fcad3f7c9461a298b442640755e4a298423da
morkalork <maffelu@gmail.com>
2017-12-06 08:36:08 +0100

    Day 6, part 2

--- src/Day6.kt
+++ src/Day6.kt
@@ -2,18 +2,25 @@ package com.morkalork.adventOfCode
 
 class Day6 {
 
-    fun solve1() {
+    fun getInput(): MutableList<Int> {
+
         // val input = "0\t2\t7\t0" // Test input
         val input = "11\t11\t13\t7\t0\t15\t5\t5\t4\t4\t1\t1\t7\t1\t15\t11"
 
-        val blocks = input
+        return input
                 .split("\\t".toRegex())
                 .map { it.toInt() }
                 .toMutableList()
+    }
 
+    fun solve1() {
+        val blocks = getInput()
         var done = false
         val passedBlocks = mutableListOf<List<Int>>()
+        var count = 0
+        var matchedIndex = -1
         while (!done) {
+            count++
             var topDog = blocks.max() ?: -1
 
             var indexer = blocks.indexOf(topDog) // Returns the first index of our "top dog"
@@ -32,13 +39,15 @@ class Day6 {
             // println("$blocks, $passedBlocks")
             if (passedBlocks.contains(blocks)) {
                 done = true
+                matchedIndex = passedBlocks.indexOf(blocks) + 1
             }
 
             val newBlock = blocks.toList()
             passedBlocks.add(newBlock)
         }
 
-        println("It took ${passedBlocks.size} attempts to get it right!")
+        println("Solution 1: ${passedBlocks.size}")
+        println("Solution 2: ${passedBlocks.size - matchedIndex} ($matchedIndex)")
     }
 
     fun solve2() {
