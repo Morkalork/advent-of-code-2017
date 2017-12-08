d5f78e733518be7b1c35e4e0065d52a6ff169ef2
morkalork <maffelu@gmail.com>
2017-12-08 08:44:27 +0100

    Final commit, ran out of time...

---
+++ src/Day8.kt
@@ -0,0 +1,25 @@
+package com.morkalork.adventOfCode
+
+import java.io.File
+
+class Day8 {
+
+    fun getInput(): List<String> {
+        val inputStream = File("src/Day8.txt").inputStream()
+        return inputStream.bufferedReader().use { it.readLines() }
+    }
+
+    fun solve1() {
+        // I DON'T HAVE ANY MOAR TIME!
+        println("This is the end...")
+    }
+
+    fun solve2() {
+        // AOC 2018, I'LL TRY TO SET ASIDE MOAR TIMEH!
+    }
+
+    fun run() {
+        solve1()
+        solve2()
+    }
+}
--- src/app.kt
+++ src/app.kt
@@ -1,6 +1,6 @@
 import com.morkalork.adventOfCode.*
 
 fun main(args: Array<String>) {
-    val day = Day7()
+    val day = Day8()
     day.run()
 }
